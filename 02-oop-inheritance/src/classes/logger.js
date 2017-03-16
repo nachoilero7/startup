232c6e813a9b58cd0a9910f7ecf59d8137700cc9
nachoilero7 <nachoilero7@gmail.com>
2017-03-16 00:12:33 -0300

    Ultimos ejercicios del topic2 un poco modularizados. Falta hacer andar Babel.

---
+++ 02-oop-inheritance/src/classes/logger.js
@@ -0,0 +1,8 @@
+"use strict";
+class Logger{
+
+	log(){
+		console.log("The PLAY event have been emmited! From the Logger...");
+	}
+}
+export default Logger;
---
+++ 02-oop-inheritance/src/classes/movie.js
@@ -0,0 +1,33 @@
+"use strict";
+import EventEmitter from './eventEmitter';
+import Actor from './actor';
+
+class Movie extends EventEmitter{
+
+	constructor(title, year, duration) {
+		super();
+		this.title = title;
+		this.year = year;
+		this.duration = duration;
+		//KEYPOINT 8.Add a method to Movie that will allow to add one or more actors at the same time.
+		this.cast = [];
+	}
+	addCast(actors){
+		let act = actors;
+		if (act.length != 0){
+			act.forEach((Actor) => {
+				this.cast.push(Actor);
+			});
+		}
+	}
+	play() {
+		this.emit('play');
+	}
+	pause(){
+		this.emit('pause');
+	}
+	resume(){
+		this.emit('resume');
+	}
+}
+export default Movie;
